6635cb4360fd679c1d2917dbc98c5f1df85d8a97
Your Name <you@example.com>
2026-03-13 09:39:05 +0900

    react

---
+++ frontend/src/App.js
@@ -0,0 +1,6 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import React from 'react';
+function App() {
+    return (_jsx("div", { children: _jsx("h1", { children: "Hello, React!" }) }));
+}
+export default App;
